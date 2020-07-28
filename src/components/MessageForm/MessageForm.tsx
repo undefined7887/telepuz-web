94223c27e5788bc550680c1568e985e85f6af6ac
undefined7887 <undefined7887@gmail.com>
2020-07-28 12:33:15 +0300

    Fixed main page styles

--- src/components/MessageForm/MessageForm.tsx
+++ src/components/MessageForm/MessageForm.tsx
@@ -25,23 +25,27 @@ export default class MessageForm extends React.Component<Props> {
 
     private onInputKeyUp(event: KeyboardEvent) {
         if (event.key === "Enter") {
-            this.props.onReady?.()
             this.printing = false
             clearTimeout(this.timer)
+            this.props.onReady?.()
             this.props.onStopPrinting?.()
             return
         }
 
         if (this.printing) {
+            console.log("Already printing")
             clearTimeout(this.timer)
         } else {
+            console.log("Not printing")
             this.printing = true
             this.props.onStartPrinting?.()
         }
 
         this.timer = setTimeout(() => {
+            console.log("Timeout")
             this.printing = false
             this.props.onStopPrinting?.()
+            clearTimeout(this.timer)
         }, PRINTING_TIMEOUT)
     }
 
